eb8649c649e216dd985b5102b3dcbec8fd88dca5
ovi-tsb <ovi_tsb@yahoo.com>
2024-01-15 15:49:40 -0500

    Deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-  
+  root 'static_pages#landing_page'
+
+
+  # get 'static_pages/landing_page'
+  # get 'static_pages/privacy_policy'
+  get "privacy_policy", to: "static_pages#privacy_policy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
