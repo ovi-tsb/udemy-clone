1db3fc2f0a0502e82134a17a350369040e931a9b
ovi-tsb <ovi_tsb@yahoo.com>
2024-01-23 10:58:22 -0500

    Fixing aws mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "admin@online-business-solutions.ca"
+  # default from: "admin@online-business-solutions.ca"
+  default from: "Ovi <admin@online-business-solutions.ca>"
   layout "mailer"
 end
